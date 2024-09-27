ccce32e946c505d6008d31fc8520076bc9e244d6
Abdoulaye <layediallokaba@gmail.com>
2024-09-27 18:06:25 +0000

    premier commit

---
+++ src/test/java/kaba/koto/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package kaba.koto.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
